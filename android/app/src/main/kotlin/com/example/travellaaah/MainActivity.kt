89df30ddad93226168f2714a69a65a8c45dd04be
aLearningLad <mocmanca@gmail.com>
2025-01-17 00:36:14 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/travellaaah/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.travellaaah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("This is a div, bruv")],
+        ),
+      ),
+    );
+  }
+}
