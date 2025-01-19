4eb01e12ed2eea027c7a3e1d88f9d92448003578
aLearningLad <mocmanca@gmail.com>
2025-01-19 23:30:21 +0200

    coordinate retrieval for location in weather base url

--- lib/services/get_location.dart
+++ lib/services/get_location.dart
@@ -1,6 +1,18 @@
+import 'package:geolocator/geolocator.dart';
+
 class GetLocation {
   // 1. will return name of location (string)
+  Future<Set<double>> getCurrentCity() async {
+    // get location permission from device
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
 
-  // 2. take this string, convert it to coordinates. Pass longitude &
-  //latitude values to base url of get weather method
+    // once permission granted, get position's longitude & latitude
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    return {position.longitude, position.latitude};
+  }
 }
